f8619f518fe881f52cdc2b0d913060d2f7811335
Katerina Mackova <ka.tie@email.cz>
2021-09-03 16:12:27 +0200

    Added pseudocode of comparison of two datasets

--- scripts/score_computation/actor_model_interface.py
+++ scripts/score_computation/actor_model_interface.py
@@ -3,6 +3,7 @@ import sys
 
 # DO NOT REMOVE
 # Adding the higher level directory (scripts/) to sys.path so that we can import from the other folders
+
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 
 from scripts.evaluate_classifier import train_classifier, evaluate_classifier, setup_classifier
@@ -11,13 +12,21 @@ from scripts.score_computation.dataset_handler import preprocess_data
 
 # Load dataset and train and save model
 def main(**kwargs):
-    load_data_and_train_classifier('data/wdc_dataset/dataset/preprocessed', 'DecisionTree')
-    matching_pairs = load_and_evaluate_model('data/wdc_dataset/dataset/preprocessed', 'DecisionTree')
+    # load_data_and_train_classifier('data/wdc_dataset/dataset/preprocessed', 'DecisionTree')
+    matching_pairs = load_model_and_predict_matches('data/wdc_dataset/dataset/preprocessed', 'DecisionTree')
     print(matching_pairs)
 
 
-def find_matching_products(product, dataset):
-    pass
+def find_matching_products(dataset1, dataset2, classifier):
+    classifier = setup_classifier(classifier, 'scripts/classifier_parameters/linear.json')
+    classifier = classifier.load()
+    for product in dataset1.rows():
+        data_subset = dataset2[dataset2['price'] / 2 <= product['price'] <= dataset2['price'] * 2]
+        # TODO: create pairs of product and each row from dataset and then predict instead of following line
+        data = data_subset
+        # TODO: # preprocess_data dataset2 and product
+        data['predicted_match'], data['predicted_scores'] = classifier.predict(data)
+    return data[data['predicted_match'] == 1]
 
 
 def load_data_and_train_classifier(dataset_folder, classifier):
@@ -35,7 +44,7 @@ def load_data_and_train_classifier(dataset_folder, classifier):
     classifier.save()
 
 
-def load_and_evaluate_model(dataset_folder, classifier):
+def load_model_and_predict_matches(dataset_folder, classifier):
     """
     Load model and unlabeled dataset and predict pairs
     @param dataset_folder: folder containing test data
