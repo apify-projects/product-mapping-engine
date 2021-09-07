05aaad4669a2b650559457fe65f380216fd1d0c3
Katerina Mackova <ka.tie@email.cz>
2021-09-07 14:52:34 +0200

    Cleaning up in actor model interface finished, jupijej!

--- scripts/score_computation/actor_model_interface.py
+++ scripts/score_computation/actor_model_interface.py
@@ -19,9 +19,9 @@ def main(**kwargs):
     output_file = 'data/extra_dataset/dataset/preprocessed/matching_pairs.csv'
     dataset1 = 'data/extra_dataset/dataset/source/amazon.csv'
     dataset2 = 'data/extra_dataset/dataset/source/extra.csv'
-    matching_pairs = find_matching_products(pd.read_csv(dataset1),
-                                            pd.read_csv(dataset2),
-                                            'DecisionTree')
+    matching_pairs = load_model_create_dataset_and_predict_matches(pd.read_csv(dataset1),
+                                                                   pd.read_csv(dataset2),
+                                                                   'DecisionTree')
     matching_pairs.to_csv(output_file, index=False)
 
 
@@ -42,9 +42,9 @@ def filter_products_with_no_similar_words(product, dataset):
     return data_subset
 
 
-def find_matching_products(dataset1, dataset2, classifier):
+def load_model_create_dataset_and_predict_matches(dataset1, dataset2, classifier):
     """
-    For each product in first dataset predict same products in the second dataset
+    For each product in first dataset find same products in the second dataset
     @param dataset1: Source dataset of products
     @param dataset2: Target dataset with products to be searched in for the same products
     @param classifier: Classifier used for product matching
@@ -56,33 +56,52 @@ def find_matching_products(dataset1, dataset2, classifier):
     dataset1['price'] = pd.to_numeric(dataset1['price'])
     dataset2['price'] = pd.to_numeric(dataset2['price'])
     for idx, product in dataset1.iterrows():
-        data_subset = dataset2[
-            (dataset2['price'] / 2 <= product['price']) & (product['price'] <= dataset2['price'] * 2)]
-        # data_subset = data_subset[data_subset['category'] != data_subset['category']]
-
-        # create pairs of product and each row from dataset
-        data_subset = filter_products_with_no_similar_words(product, data_subset)
-        data_subset = data_subset.rename(columns=lambda s: s + '2')
-        rows = len(data_subset.index)
-        data = pd.DataFrame()
-        for _ in range(0, rows):
-            data = data.append(product, ignore_index=True)
-        data = data.rename(columns=lambda s: s + '1')
-        data.reset_index(drop=True, inplace=True)
-        data_subset.reset_index(drop=True, inplace=True)
-        data = pd.concat([data, data_subset], axis=1)
-        data = data.rename(columns={'url1': 'id1', 'url2': 'id2'})
+        data_subset = filter_products(product, dataset2)
+        data = create_dataset_for_predictions(product, data_subset)
         # preprocess data
         images_folder = None
         data_prepro = preprocess_data_without_saving(data, images_folder)
-
         data['predicted_match'], data['predicted_scores'] = classifier.predict(data_prepro)
         predicted_matches = predicted_matches.append(
             data[data['predicted_match'] == 1][['name1', 'id1', 'name2', 'id2', 'predicted_scores']])
     return predicted_matches
 
 
-def load_data_and_train_classifier(dataset_folder, classifier):
+def create_dataset_for_predictions(product, maybe_the_same_products):
+    """
+    Create one dataset for model to predict matches that will consist of following pairs: given product with every product from the dataset of possible matches
+    @param product: product to be compared with all products in the dataset
+    @param maybe_the_same_products: dataset of products that are possibly the same as given product
+    @return: one dataset for model to predict pairs
+    """
+    maybe_the_same_products = maybe_the_same_products.rename(columns=lambda s: s + '2')
+    final_dataset = pd.DataFrame()
+    for _ in range(0, len(maybe_the_same_products.index)):
+        final_dataset = final_dataset.append(product, ignore_index=True)
+    final_dataset = final_dataset.rename(columns=lambda s: s + '1')
+    final_dataset.reset_index(drop=True, inplace=True)
+    maybe_the_same_products.reset_index(drop=True, inplace=True)
+    final_dataset = pd.concat([final_dataset, maybe_the_same_products], axis=1)
+    final_dataset = final_dataset.rename(columns={'url1': 'id1', 'url2': 'id2'})
+    return final_dataset
+
+
+def filter_products(product, dataset):
+    """
+    Filter products in dataset according to the price, category and word similarity to reduce number of comparisons
+    @param product: given product for which we want to filter dataset
+    @param dataset: dataset of products to be filtered
+    @return: Filtered dataset of products that are possibly the same as given product
+    """
+    data_filtered = dataset[
+        (dataset['price'] / 2 <= product['price']) & (product['price'] <= dataset['price'] * 2)]
+    if 'category' in dataset:
+        data_filtered = data_filtered[data_filtered['category'] != data_filtered['category']]
+    data_filtered = filter_products_with_no_similar_words(product, data_filtered)
+    return data_filtered
+
+
+def load_data_and_train_model(dataset_folder, classifier):
     """
     Load dataset and train and save model
     @param dataset_folder: folder containing data
@@ -99,7 +118,7 @@ def load_data_and_train_classifier(dataset_folder, classifier):
 
 def load_model_and_predict_matches(dataset_folder, classifier):
     """
-    Load model and unlabeled dataset and predict pairs
+    Directly load model and already created unlabeled dataset with product pairs and predict pairs
     @param dataset_folder: folder containing test data
     @param classifier: classifier type
     @return: pair indices of matches
