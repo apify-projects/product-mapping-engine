6b297fe69ff1debfd99efa3bf9594707085d8779
Katerina Mackova <ka.tie@email.cz>
2022-03-21 13:09:14 +0100

    Deleted useless files

--- data/vocabularies/corpus/vocabulary_cleaner.py
+++ data/vocabularies/corpus/vocabulary_cleaner.py
@@ -1,10 +1,27 @@
 import csv
 import json
 import time
-
+import click
 import requests
 
 
+@click.command()
+@click.option('--batch_size', '-b',
+              default='500',
+              required=False,
+              type=int,
+              help='Batch size to be preprocessed')
+@click.option('--language', '-l',
+              default='cz',
+              required=False,
+              help='Language of input vocabulary')
+@click.option('--input_file', '-i',
+              default='data/vocabularies/corpus/preprocessed/cz_dict.csv',
+              required=False,
+              help='Input vocabulary dictionary file to preprocess')
+@click.option('--output_file', '-o',
+              default='data/vocabularies/corpus/preprocessed/cz_dict_cleaned.txt',
+              required=False, help='Output file with valid uniques words.')
 def lemmatize_batch(batch_str, language):
     """
     Lemmatize batch of words using Morphoditta
@@ -69,3 +86,14 @@ def save_vocabulary(words, output_file):
     with open(output_file, 'w', encoding='utf-8') as f:
         for w in words:
             f.write(f"{w}\n")
+
+
+# Load vocabulary and create vocabulary with unique valid words
+# Switch to load czech or english dictionary to process
+def main(**kwargs):
+    vocabulary = load_and_lemmatize_dictionary(kwargs['input_file'], kwargs['batch_size'], kwargs['language'])
+    save_vocabulary(vocabulary, kwargs['output_file'])
+
+
+if __name__ == '__main__':
+    main()
