4753dfe6b2a73cea0db12b04dd69245a8f3ff305
kackamac <ka.tie@email.cz>
2022-05-18 13:59:56 +0200

    Corrected errors

--- engine/scripts/actor_model_interface.py
+++ engine/scripts/actor_model_interface.py
@@ -281,8 +281,9 @@ def evaluate_executor_results(classifier, preprocessed_pairs, task_id, data_type
         labeled_dataset['birthdate'] = [datetime.today().strftime('%Y-%m-%d')] * len(labeled_dataset)
 
     # remove data_to_remove from labeled_dataset
-    labeled_dataset = labeled_dataset.merge(data_to_remove[['id1', 'id2']], on=['id1', 'id2'], how='left', indicator=True)
-    labeled_dataset = labeled_dataset[labeled_dataset['_merge'] == 'left_only'].drop(columns='_merge')
+    if data_to_remove is not None and len(data_to_remove) != 0:
+        labeled_dataset = labeled_dataset.merge(data_to_remove[['id1', 'id2']], on=['id1', 'id2'], how='left', indicator=True)
+        labeled_dataset = labeled_dataset[labeled_dataset['_merge'] == 'left_only'].drop(columns='_merge')
 
     matching_pairs = labeled_dataset[['id1', 'id2', 'match']]
     predicted_pairs = preprocessed_pairs[['id1', 'id2', 'predicted_scores', 'predicted_match']]
